b017fcf01d1ccfac8594ca656133975c401f582b
jotdhessi <harmanjot28082004@gmail.com>
2023-11-28 15:04:58 -0500

    version4

--- Program1.py
+++ Program1.py
@@ -4,8 +4,15 @@ b=input("Has the patient been here before? (y/n)")
 c=int(input("What is the patient height (in cm)?"))
 d=float(input("What is the patient weight (in kg)?"))
 e=input("When was the patient last weighed in (1/1/2000 if never weighed)?")
-f=float(input("What was the patient weight (in kg, -1 if never weighed)?"))
+f=float(input("What was the patient weight ?"))
 g=int(input("Practitioner overall assessment of the patient health (-5=very poor to +5=excellent, 0 for neutral)"))
+
+
+height=int(input("Height in cm"))
+weight=float(input("Weight in kg"))
+bmi=(weight/ (height**2))*10000
+
+
 
 
 
