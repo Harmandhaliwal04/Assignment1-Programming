b88d5fdc3105e802e52eb2b65327773f7620ef0f
jotdhessi <harmanjot28082004@gmail.com>
2023-11-28 14:46:49 -0500

    version2

--- Program1.py
+++ Program1.py
@@ -1,13 +1,11 @@
+print(" Melanie Dental Clinic")
 a=input("Patient's name:")
-print("The patient name is",a)
 b=input("Has the patient been here before? (y/n)")
-c=int(input("What is the patient’s height (in cm)?"))
-print("The height of patient is",c)
-d=float(input("What is the patient’s weight (in kg)?"))
-print("The weight of the patient is",d)
+c=int(input("What is the patient height (in cm)?"))
+d=float(input("What is the patient weight (in kg)?"))
 e=input("When was the patient last weighed in (1/1/2000 if never weighed)?")
+f=float(input("What was the patient weight (in kg, -1 if never weighed)?"))
+g=int(input("Practitioner overall assessment of the patient health (-5=very poor to +5=excellent, 0 for neutral)"))
 
-f=float(input("What was the patient’s weight (in kg, -1 if never weighed)?"))
-g=int(input("Practitioner’s overall assessment of the patient’s health (-5=very poor to +5=excellent, 0 for neutral)"))
 
 
